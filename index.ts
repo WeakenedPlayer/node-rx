85e9c7b25564ac4c2c578a14712fbc0a35088711
WeakenedPlayer <weakenedplayer@gmail.com>
2017-08-26 12:10:24 +0900

    ファイル構成変更

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';  // https://github.com/redux-observable/redux-observable/issues/78
 
-export * from './node-rx';
-export { Action, nextState, toAction } from './store';
-export { Actions, Effect, toPayload } from './effects';
+export * from './src/node-rx';
+export { Action, nextState, toAction } from './src/store';
+export { Actions, Effect, toPayload } from './src/effects';
