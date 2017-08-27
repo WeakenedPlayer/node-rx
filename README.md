2b807669c1107b8d806fce7a3a8887d7a53bfc72
WeakenedPlayer <weakenedplayer@gmail.com>
2017-08-28 00:36:50 +0900

    公開範囲修正

---
+++ README.md
@@ -0,0 +1,2 @@
+## NodeRx
+### Modified ngrx for node.js. Based on @ngrx/store 2.2.2, @ngrx/effects/2.0.3. (https://github.com/ngrx)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';  // https://github.com/redux-observable/redux-observable/issues/78
 
 export * from './node-rx';
-export { Action, nextState, toAction } from './store';
+export { Action, nextState, toAction, Store, State } from './store';
 export { Actions, Effect, toPayload } from './effects';
